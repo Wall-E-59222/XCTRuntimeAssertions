2ab10cc999cb9a544aa99a1422997f07475368ed
Wall-E-59222 <akademi59222@gmail.com>
2025-01-29 20:26:11 -0500

    modify

--- Sources/XCTRuntimeAssertions/XCTRuntimeAssertionInjector.swift
+++ Sources/XCTRuntimeAssertions/XCTRuntimeAssertionInjector.swift
@@ -11,7 +11,7 @@ import Foundation
 
 
 private final class RuntimeInjections: Sendable {
-    private nonisolated var injected: [XCTRuntimeAssertionInjector] = []
+    private var injected: [XCTRuntimeAssertionInjector] = []
     private let lock = NSLock()
 
     @inlinable var isEmpty: Bool {
